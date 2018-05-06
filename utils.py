d5fdcc6d8d25262209a29e179561bf123e17e154
fjw_Code <1748202178@qq.com>
2018-05-06 19:58:08 +0800

    The utils methods.

--- utils.py
+++ utils.py
@@ -1,16 +1,12 @@
 import imageio
 
-ims = [ ]
-for i in range(1, 387):
-    ims.append(imageio.imread('png/{}.png'.format(i)))
 
-imageio.mimsave('life.gif', ims, duration=0.3)
+def synthetic_gif(num):
+    ims = []
+    for i in range(1, num+1):
+        ims.append(imageio.imread('png/{}.png'.format(i)))
+    imageio.mimsave('life.gif', ims, duration=0.3)
 
 
-# from PIL import Image
-
-# gif = Image.open('png/1.png')
-# ims = [ ]
-# for i in range(1, 387):
-#     ims.append(Image.open('png/{}.png'.format(i)))
-# gif.save('life5.gif', save_all=True, append_images=ims, loop=1, duration=0.5)
+if __name__ == '__main__':
+    synthetic_gif(400)
