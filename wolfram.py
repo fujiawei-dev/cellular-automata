83c59682ad3d61fe2c4f8f9021cbcc6f96326562
fjw_Code <1748202178@qq.com>
2018-06-14 08:48:41 +0800

    Wolfram elementary one-dimensional cellular automaton.

--- wolfram.py
+++ wolfram.py
@@ -1,8 +1,6 @@
 '''Wolfram elementary one-dimensional cellular automaton.'''
 
-
 mapping = {0: ' ', 1: '\u2588'}
-
 
 def rule30(M):
     N = [0] * len(M)
@@ -31,6 +29,11 @@ def rule90(M):
 if __name__ == '__main__':
     length = 15
     M = [0]*length + [1] + [0]*(length+1)
+
+    # for i in range(16):
+    #     print(' '.join(map(lambda x: mapping[x], M)))
+    #     M = rule30(M)
+
     for i in range(16):
         print(' '.join(map(lambda x: mapping[x], M)))
         M = rule90(M)
